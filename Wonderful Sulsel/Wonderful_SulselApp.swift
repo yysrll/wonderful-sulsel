ffa255d87cfb6a7536c85291db3e8aacbec504ec
yysrll <yusril.uzumaki@gmail.com>
2022-06-10 17:13:28 +0800

    Initial Commit

---
+++ Wonderful Sulsel/Wonderful_SulselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wonderful_SulselApp.swift
+//  Wonderful Sulsel
+//
+//  Created by Yusril on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Wonderful_SulselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
