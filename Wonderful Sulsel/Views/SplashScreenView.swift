9ba74df6619f6d7afbff8f6d23de8845add623c5
yysrll <yusril.uzumaki@gmail.com>
2022-06-11 15:48:01 +0800

    feat: implement a splashscreen

---
+++ Wonderful Sulsel/Views/SplashScreenView.swift
@@ -0,0 +1,50 @@
+//
+//  SplashScreenView.swift
+//  Wonderful Sulsel
+//
+//  Created by Yusril on 11/06/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image(systemName: "paperplane.fill")
+                        .font(.system(size: 70))
+                        .foregroundColor(.green)
+                    Text("Wonderful Sulsel")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
